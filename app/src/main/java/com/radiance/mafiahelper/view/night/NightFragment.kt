0e55ef6fb742df45e50d1fa8b8cbc5fc7956b6cd
Kesso <kesso@bsvt-nt.by>
2019-11-18 17:00:08 +0300

    add night fragment

--- app/src/main/java/com/radiance/mafiahelper/view/night/NightFragment.kt
+++ app/src/main/java/com/radiance/mafiahelper/view/night/NightFragment.kt
@@ -6,16 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.radiance.mafiahelper.R
+import com.radiance.mafiahelper.emptyGame
+import com.radiance.mafiahelper.inflate
+import com.radiance.mafiahelper.player.Player
+import com.radiance.mafiahelper.player.PlayerHolder
+import com.radiance.mafiahelper.view.adapter.Holder
+import com.radiance.mafiahelper.view.adapter.HolderBuilder
+import com.radiance.mafiahelper.view.adapter.RecyclerAdapter
+import kotlinx.android.synthetic.main.night_fragment.*
 
 class NightFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = NightFragment()
-    }
-
     private lateinit var viewModel: NightViewModel
+    private var game = emptyGame()
+    private lateinit var adapter: RecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,6 +33,47 @@ class NightFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        arguments?.let {
+            val args = NightFragmentArgs.fromBundle(it)
+            game = args.game
+        }
+
         viewModel = ViewModelProviders.of(this).get(NightViewModel::class.java)
+        viewModel.currentPlayer.observe(this, androidx.lifecycle.Observer { updateCurrentPlayer() })
+
+        adapter = RecyclerAdapter(createPlayerHolders(), NightHolderBuilder(viewModel))
+        n_recycler.layoutManager = LinearLayoutManager(context)
+        n_recycler.adapter = adapter
+
+        viewModel.init(game, findNavController())
+
+    }
+
+    private fun updateCurrentPlayer() {
+        adapter.setData(createPlayerHolders())
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun createPlayerHolders(): ArrayList<PlayerHolder> {
+        val answer = ArrayList<PlayerHolder>()
+
+        for (player in viewModel.players){
+            answer.add(
+                PlayerHolder(
+                    player = player,
+                    isSelected = player == viewModel.currentPlayer.value
+                )
+            )
+        }
+
+        return answer
+    }
+
+    private class NightHolderBuilder(private val viewModel: NightViewModel): HolderBuilder {
+        override fun build(parent: ViewGroup): Holder {
+            val inflatedView = parent.inflate(R.layout.night_item, false)
+            return NightViewHolder(inflatedView, viewModel)
+        }
     }
 }
